e79a6c9664ade8aeb642ff156fbc2689c4548f7c
jmoller555 <jacquelyn.moller@gmail.com>
2015-10-20 18:56:38 +0000

    Add styling to vote form

--- core/views.py
+++ core/views.py
@@ -24,6 +24,12 @@ class QuestionListView(ListView):
   template_name = "question/question_list.html"
   paginate_by = 5
 
+  def get_context_data(self, **kwargs):
+    context = super(QuestionListView, self).get_context_data(**kwargs)
+    user_votes = Question.objects.filter(vote__user=self.request.user)
+    context['user_votes'] = user_votes
+    return context
+
 class QuestionDetailView(DetailView):
   model = Question
   template_name = 'question/question_detail.html'
@@ -35,6 +41,8 @@ class QuestionDetailView(DetailView):
     context['answers'] = answers
     user_answers = Answer.objects.filter(question=question, user=self.request.user)
     context['user_answers'] = user_answers
+    user_votes = Answer.objects.filter(vote__user=self.request.user)
+    context['user_votes'] = user_votes
     return context
 
 class QuestionUpdateView(UpdateView):
@@ -139,7 +147,7 @@ class UserDetailView(DetailView):
     user_in_view = User.objects.get(username=self.kwargs['slug'])
     questions = Question.objects.filter(user=user_in_view).exclude(visibility=1)
     context['question'] = questions
-    answers = Answers.objects.filter(user=user_in_view).exclude(visibility=1)
+    answers = Answer.objects.filter(user=user_in_view).exclude(visibility=1)
     context['answers'] = answers
     return context
 
